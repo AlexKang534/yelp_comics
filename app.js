a58fc770be0f10ca4cc22be97937d751642938f7
Alex Kang <alexkang534@gmail.com>
2021-09-16 03:48:04 +0000

    Fixed app.js and server not loading

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(express.static('public'));
 
 //Express Session Config
 app.use(expressSession({
-	secret: process.env.ES_SECRET | config.expressSession.secret,
+	secret: process.env.ES_SECRET || config.expressSession.secret,
 	resave: false,
 	saveUninitialized: false
 }));
